d64cb926bbe9b34aad52ec2fd83461288bc585f9
crossfit00 <wjdrbs956@gmail.com>
2025-05-24 21:14:13 +0900

    feat: Slf4j2 추가

---
+++ src/main/kotlin/com/example/study/common/Slf4j2KotlinLogging.kt
@@ -0,0 +1,14 @@
+package com.example.study.common
+
+import mu.KotlinLogging
+
+/**
+ * Slf4j2KotlinLogging
+ *
+ * @author JungGyun.Choi
+ * @version 1.0.0
+ * @since 2025. 05. 23.
+ */
+object Slf4j2KotlinLogging {
+    val log = KotlinLogging.logger {}
+}
